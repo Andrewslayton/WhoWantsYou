daaabad2716626378e273017fb88c46cf2b7226b
Andrewslayton <slayton@DESKTOP-QS1UE0R.localdomain>
2023-03-25 14:46:44 -0400

    edited register to flash an error message when username is taken. started developing a proper login function with password hash

--- src/web.py
+++ src/web.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for,flash 
 from werkzeug.utils import secure_filename
-from flask_login import LoginManager, current_user, login_required
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 import os 
-
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY') or 'my_secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/images'
 
 from database import get_db, init_db
+from modelUser import User
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -43,17 +44,32 @@ def register():
         user = db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
         if user is not None:
             error = 'Username already taken.'
+            flash('Sorry, username already taken.')
             return render_template('register.html', error=error)
         db.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
         db.commit()
         return redirect('/login')
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        user = db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+        if user is None:
+            error = 'Invalid username or password.'
+            return render_template('login.html', error=error)
+        if not User.check_password(password, user['password']):
+            error = 'Invalid username or password.'
+            return render_template('login.html', error=error)
+        login_user(User(user['id'], user['username'], user['password']), remember=True)
+        return redirect(url_for('profiles'))
     return render_template('login.html')
         
 @app.route('/profiles')
+@login_required
 def profiles():
     db = get_db()
     c = db.cursor()
